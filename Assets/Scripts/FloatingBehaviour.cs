110232f3e5421cf2fc68d45257e4a2306732fe2c
Austin Lancaster <aus@lancaster@gmail.com>
2019-09-15 18:58:55 +1000

    Implement floating behaviour

---
+++ Assets/Scripts/FloatingBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingBehaviour : MonoBehaviour
+{
+
+    [SerializeField]
+    float amount;
+    [SerializeField]
+    float periodLength;
+
+    float posY;
+    float offset;
+
+    void Start()
+    {
+        posY = transform.position.y;
+    }
+
+    void Update()
+    {
+        offset = amount * Mathf.Sin((2 * Mathf.PI * Time.time)/periodLength);
+        transform.position = new Vector3(
+            transform.position.x, 
+            posY + offset,
+            transform.position.z
+            );
+    }
+}
