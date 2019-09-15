24136fe9142c655263b29aa6ce9ccfaab3724859
Austin Lancaster <aus@lancaster@gmail.com>
2019-09-15 20:18:24 +1000

    Implement blobbing behaviour

---
+++ Assets/Scripts/BlobbingBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobbingBehaviour : MonoBehaviour
+{
+
+    [SerializeField]
+    float amount;
+    [SerializeField]
+    float periodLength;
+
+    Vector3 baseScale;
+
+    void Start()
+    {
+        baseScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        Vector3 scaleAmount = new Vector3(
+            amount + 1 + amount * Mathf.Sin((2 * Mathf.PI * Time.time) / periodLength),
+            1,
+            amount + 1 + amount * Mathf.Sin((2 * Mathf.PI * Time.time + Mathf.PI) / periodLength)
+            );
+
+        transform.localScale = Vector3.Scale(baseScale, scaleAmount);
+    }
+}
