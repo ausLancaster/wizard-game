395b1e10aa96c46e1eb36c68f1e0a05b9c15f8df
Austin Lancaster <aus@lancaster@gmail.com>
2019-09-19 21:49:02 +1000

    Implement basic throwing

---
+++ Assets/Scripts/PotionBreak.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionBreak : MonoBehaviour
+{
+    [SerializeField]
+    Transform breakEffect;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Surface")) {
+
+            Transform effect = Instantiate(breakEffect);
+            effect.transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+            Destroy(gameObject);
+        }
+
+    }
+}
--- Assets/Scripts/ThrowPotion.cs
+++ Assets/Scripts/ThrowPotion.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ThrowPotion : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    Transform potionPrefab;
+    [SerializeField]
+    Vector3 throwDirection = new Vector3(0, 1, 1).normalized;
+    [SerializeField]
+    float throwSpeed = 6;
+    [SerializeField]
+    Vector3 startingPos = new Vector3(0, 1f, 0);
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Throw ();
+        }
+    }
+
+    void Throw()
+    {
+        Transform potion = Instantiate(potionPrefab);
+        potion.transform.position = transform.position + startingPos;
+        potion.GetComponent<Rigidbody>().velocity = transform.rotation * (throwSpeed * throwDirection);
     }
 }
