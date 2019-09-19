930d691d38041315cd917d67e56090a3b5a50b23
Jessey Fung <h.fung@student.unimelb.edu.au>
2019-09-19 14:55:16 +1000

    Upload New File

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 2.0f;
+    public float rotationSpeed = 0.15f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player movement and rotation
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 newPos = new Vector3(h, 0.0f, v);
+
+        // Slerp interpolates the rotation between current rotation and movement rotation for smoother turns
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(newPos), rotationSpeed);
+        transform.Translate(newPos * moveSpeed * Time.deltaTime, Space.World);
+
+
+
+    }
+}
