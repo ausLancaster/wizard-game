ea61f3fd02d13fce172b524a0cab2674b59641d9
skite <skite@student.unimelb.edu.au>
2019-09-26 14:17:17 +1000

    Enemy collision with potions and health

---
+++ Assets/PotionCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionCollision : MonoBehaviour
+{
+    public GameObject potion;
+    EnemyHealth enemyHealth;
+
+    private void Awake()
+    {
+        enemyHealth = GetComponent<EnemyHealth>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // https://learn.unity.com/tutorial/collecting-scoring-and-building-the-game?projectId=5c51479fedbc2a001fd5bb9f#5c7f8529edbc2a002053b78b
+        if (other.gameObject.CompareTag("Potion"))
+        {
+            //this.gameObject.SetActive(false);
+            // https://answers.unity.com/questions/176001/destroy-on-collision.html
+            //Destroy(this.gameObject);
+            Debug.Log("collision detected");
+            enemyHealth.DamageTaken(25);
+        }
+    }
+}
