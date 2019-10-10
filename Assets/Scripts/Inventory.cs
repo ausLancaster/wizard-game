0722890cba3b016d230064bbc6cb9076523c0b71
skite <skite@student.unimelb.edu.au>
2019-10-10 14:49:26 +1100

    basic ui

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject item;
+    public int leafCount = 0;
+
+    public Text inventoryCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventoryCount.text = leafCount.ToString();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inventoryCount.text = leafCount.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collision");
+
+        if (other.gameObject.CompareTag("Item"))
+        {
+            Debug.Log("item found");
+            if (other.gameObject.name == "Leaf")
+            {
+                leafCount++;
+                Debug.Log("current leaves " + leafCount);
+                inventoryCount.text = leafCount.ToString();
+            }
+
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UseItem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseItem : MonoBehaviour
+{
+    //public GameObject leaf;
+    Inventory Inventory;
+
+    private void Awake()
+    {
+        Inventory = GetComponent<Inventory>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) == true)
+        {
+            if (Inventory.leafCount > 0)
+            {
+                Debug.Log("test");
+                Inventory.leafCount--;
+            }
+            
+
+        }
+        
+    }
+}
