55bf1394ac51a6c5ff6b06bd80619cc9fda1fb0d
Jessey Fung <h.fung@student.unimelb.edu.au>
2019-10-11 11:43:02 +1100

    Create Name scene controller

---
+++ Assets/Scripts/SceneControllers/CreateNameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CreateNameController : MonoBehaviour
+{
+    public string playerName;
+
+    public void Start()
+    {
+        playerName = GlobalOptions.playerName;
+    }
+
+    public void InputFieldChanged()
+    {
+        GlobalOptions.playerName = playerName;
+    }
+
+    public void OnStartGameButtonPressed()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnCancelButtonPressed()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
