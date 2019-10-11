ea0395d3d43d66f333f6433522508a1d7ebabd23
Jessey Fung <h.fung@student.unimelb.edu.au>
2019-10-11 11:45:44 +1100

    Quit Game scene controller

---
+++ Assets/Scripts/SceneControllers/QuitGameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class QuitGameController : MonoBehaviour
+{
+
+    public void OnYesButtonPressed()
+    {
+        //Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void OnNoButtonPressed()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
