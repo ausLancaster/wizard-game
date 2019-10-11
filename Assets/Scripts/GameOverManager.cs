9a6d07c35a50c266139a152a5750f53a1f77511a
Jessey Fung <h.fung@student.unimelb.edu.au>
2019-10-11 11:46:41 +1100

    Game over manager

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public HealthManager healthManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthManager.GetHealth() <= 0)
+        {
+            SceneManager.LoadScene("CreateName");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+}
