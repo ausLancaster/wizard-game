8dd8d4fd23502dd1387e5b40ff223571c9ef80c3
Jessey Fung <h.fung@student.unimelb.edu.au>
2019-10-17 23:06:55 +1100

    Update UI_Manager.cs

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -1,15 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
     public InGameController inGameController;
+    public Canvas controlsCanvas;
+    public Canvas exitCanvas;
 
     // Use this for initialization
     void Start()
     {
-
+        GameObject controlsMenu = GameObject.Find("ControlsCanvas");
+        controlsCanvas = controlsMenu.GetComponent<Canvas>();
+        controlsCanvas.enabled = false;
+        GameObject exitPanel = GameObject.Find("ExitCanvas");
+        exitCanvas = exitPanel.GetComponent<Canvas>();
+        exitCanvas.enabled = false;
     }
 
     // Update is called once per frame
@@ -19,6 +26,35 @@ public class UI_Manager : MonoBehaviour
         {
             inGameController.TogglePauseMenu();
         }
+    }
+
+    public void ToggleControlsMenu()
+    {
+        if (controlsCanvas.enabled)
+        {
+            controlsCanvas.enabled = false;
+        }
+        else
+        {
+            controlsCanvas.enabled = true;
+        }
+    }
+
+    public void ToggleExitPanel()
+    {
+        if (exitCanvas.enabled)
+        {
+            exitCanvas.enabled = false;
+        }
+        else
+        {
+            exitCanvas.enabled = true;
+        }
+    }
+
+    public void ExitCurrentGame()
+    {
+        inGameController.ExitGame();
     }
 
 }
