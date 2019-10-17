734291f8777ce1e0d89572a88c5744c1c06ae367
Jessey Fung <h.fung@student.unimelb.edu.au>
2019-10-17 14:42:52 +1100

    Replace InGameController.cs

--- Assets/Scripts/SceneControllers/InGameController.cs
+++ Assets/Scripts/SceneControllers/InGameController.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class InGameController : MonoBehaviour
 {
-
     public HealthManager healthManager;
     public PlayerController player;
+    public UI_Manager UI;
 
     void Start()
     {
-
+        UI.GetComponentInChildren<Canvas>().enabled = false;
     }
 
     void Update()
@@ -19,12 +19,36 @@ public class InGameController : MonoBehaviour
 
     }
 
+    public void TogglePauseMenu()
+    {
+        // not the optimal way but for the sake of readability
+        if (UI.GetComponentInChildren<Canvas>().enabled)
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = false;
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = true;
+            Time.timeScale = 0f;
+        }
+    }
 
     public void GameOver()
     {
         SceneManager.LoadScene("GameOver");
     }
 
+    // To be completed
+    public void OpenControls()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("QuitGame");
+    }
 
 }
 
