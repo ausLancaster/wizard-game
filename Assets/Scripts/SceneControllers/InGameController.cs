93631725d040a35b9204b49d82edde095e4c739e
Jessey Fung <h.fung@student.unimelb.edu.au>
2019-10-17 23:07:16 +1100

    Update InGameController.cs

--- Assets/Scripts/SceneControllers/InGameController.cs
+++ Assets/Scripts/SceneControllers/InGameController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,46 +8,46 @@ public class InGameController : MonoBehaviour
     public HealthManager healthManager;
     public PlayerController player;
     public UI_Manager UI;
+    public Canvas pausedCanvas;
 
     void Start()
     {
-        UI.GetComponentInChildren<Canvas>().enabled = false;
+        GameObject pausedMenu = GameObject.Find("PausedCanvas");
+        pausedCanvas = pausedMenu.GetComponent<Canvas>();
+        pausedCanvas.enabled = false;
     }
 
     void Update()
     {
-
+        
     }
 
     public void TogglePauseMenu()
     {
         // not the optimal way but for the sake of readability
-        if (UI.GetComponentInChildren<Canvas>().enabled)
+        if (pausedCanvas.enabled)
         {
-            UI.GetComponentInChildren<Canvas>().enabled = false;
+            pausedCanvas.enabled = false;
+            UI.controlsCanvas.enabled = false;
+            UI.exitCanvas.enabled = false;
             Time.timeScale = 1.0f;
         }
         else
         {
-            UI.GetComponentInChildren<Canvas>().enabled = true;
+            pausedCanvas.enabled = true;
             Time.timeScale = 0f;
         }
     }
 
     public void GameOver()
     {
-        SceneManager.LoadScene("GameOver");
+
     }
 
-    // To be completed
-    public void OpenControls()
-    {
-        
-    }
 
-    public void QuitGame()
+    public void ExitGame()
     {
-        SceneManager.LoadScene("QuitGame");
+        SceneManager.LoadScene("MainMenu");
     }
 
 }
