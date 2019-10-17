6fbcef708189c8279f5dfc99d6e2186abf260142
Jessey Fung <h.fung@student.unimelb.edu.au>
2019-10-17 14:41:31 +1100

    Pause Menu UI

---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    public InGameController inGameController;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            inGameController.TogglePauseMenu();
+        }
+    }
+
+}
+
+   
