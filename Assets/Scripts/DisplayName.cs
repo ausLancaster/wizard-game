e68d9a76a928ff04238732d278a36af0f67204ba
Jessey Fung <h.fung@student.unimelb.edu.au>
2019-10-19 11:26:01 +1100

    name is displayed

---
+++ Assets/Scripts/DisplayName.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayName : MonoBehaviour
+{
+    public Text dispName;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        dispName = GetComponent<Text>();
+        dispName.text = "The Great and Powerful\n" + GlobalVariables.globalVariables.playerName;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
