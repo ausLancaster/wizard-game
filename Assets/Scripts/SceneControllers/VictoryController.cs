123788aa67753e022785821237556a08c6931461
jessey <h.fung@student.unimelb.edu.au>
2019-10-27 10:24:23 +1100

    victory scene shown when boss is dead

---
+++ Assets/Scripts/SceneControllers/VictoryController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class VictoryController : MonoBehaviour
+{
+
+    public void OnQuitButtonPressed()
+    {
+        //Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void OnBackButtonPressed()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
