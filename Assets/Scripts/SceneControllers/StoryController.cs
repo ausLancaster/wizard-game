b138604bf7ad146fe3fa359d2197a3aecf015c9e
jessey <h.fung@student.unimelb.edu.au>
2019-10-27 01:34:52 +1100

    fix merge

---
+++ Assets/Scripts/SceneControllers/StoryController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StoryController : MonoBehaviour
+{
+
+    public void OnBackButtonPressed()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
